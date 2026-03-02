20260489c37a14f0137f07de00a2010f5bf5f87b
Bhumi Pandhare <bhumi.pandhare27@gmail.com>
2026-03-02 15:29:06 -0600

    bio component added

---
+++ app/components/bio.jsx
@@ -0,0 +1,11 @@
+export default function bio()
+{
+    return(
+        <div>
+           <h1>Hi, I'm Bhumi!</h1> 
+           <p>Welcome to my personal website!</p>
+           <p>I'm majoring in Computer Engineering at 
+            the University of Illinois at Chicago.</p>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import bio from "./components/bio";
 
 export default function Home() {
   return (
